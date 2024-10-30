6952880c4ed014e5919e1c29163656a0568acdea
MuzzamilBukhari <muhammadmuzzamilbukhari@gmail.com>
2024-10-30 18:04:51 +0500

    xyz

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+
+module.exports = {
+  images: {
+    domains: ['images.unsplash.com', 'encrypted-tbn0.gstatic.com', 'www.mensjournal.com', 'img.freepik.com'],
+  }
+}
 const nextConfig: NextConfig = {
   /* config options here */
 };
