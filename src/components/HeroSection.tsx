2f38f3818d65852e3afe99ef565291abe9391075
huzaifanaeem1 <huzaifaneem1407@gmail.com>
2024-10-30 21:01:30 +0500

    'huzaifa-muzamil'

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
+
 const HeroSection = () => {
   return (
     <section
@@ -9,24 +10,20 @@ const HeroSection = () => {
     >
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Before they sold out
+          <h1 className="title-font text-5xl  mb-4 font-bold text-white">
+          Build Your
             <br className="hidden lg:inline-block" />
-            readymade gluten
+            Dream Physique
           </h1>
-          <p className="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+          <p className=" text-white font-medium mb-8 leading-relaxed">
+          Lorem ipsum dolor sit, bus earum, aliquam ipsa repellat iusto esse <br />
+           laudantium animi vitae consectetur obcaecati
           </p>
           <div className="flex justify-center">
-            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
+            <button className="inline-flex text-black font-medium bg-cyan-500 border-0 py-2 px-6 focus:outline-none hover:bg-cyan-600 rounded-lg  duration-200 text-lg">
+              Join us
             </button>
-            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-              Button
-            </button>
+      
           </div>
         </div>
       </div>
--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -6,7 +6,7 @@ const NavLink = ({ name }: { name: string }) => {
     <>
       <Link
         href={""}
-        className="mr-5 hover:text-gray-900 hover:bg-primary rounded-lg duration-200 px-3 py-1"
+        className="mr-5 hover:text-gray-900 hover:bg-cyan-600 rounded-lg duration-200 px-3 py-1"
       >
         {name}
       </Link>
