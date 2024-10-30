49130bab552a2265498b5260f9f49e15168d9619
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-10-30 13:46:26 +0500

    initial commit

---
+++ src/components/navbar/NavLink.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const NavLink = ({ name }: { name: string }) => {
+  return (
+    <>
+      <Link
+        href={""}
+        className="mr-5 hover:text-gray-900 hover:bg-primary rounded-lg duration-200 px-3 py-1"
+      >
+        {name}
+      </Link>
+    </>
+  );
+};
+
+export default NavLink;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import NavLink from "./NavLink";
+
+const navItems = [
+  {
+    id: 1,
+    name: "Home",
+    slug: "/",
+  },
+  {
+    id: 2,
+    name: "About us",
+    slug: "/about",
+  },
+  {
+    id: 3,
+    name: "Classes",
+    slug: "/classes",
+  },
+  {
+    id: 4,
+    name: "Trainers",
+    slug: "/trainers",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <header className="text-primary body-font bg-black">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div>
+          {/* <Image src={""} alt={""}></Image> */}
+          <Link href={"/"} className="ml-3 text-xl">
+            Xtreme Fitness
+          </Link>
+        </div>
+        <nav className="md:ml-auto flex flex-wrap items-center md:gap-4 mt-5 md:mt-0 text-base justify-center">
+          {navItems.map((item) => (
+            <NavLink key={item.id} name={item.name} />
+          ))}
+        </nav>
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          Join us
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
